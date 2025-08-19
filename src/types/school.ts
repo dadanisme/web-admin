e5acf0f3dbef5f50d8ae136376d4fe9792de43ae
Ramdan <dadanisme@upi.edu>
2025-08-19 10:56:30 +0800

    create school types

---
+++ src/types/school.ts
@@ -0,0 +1,22 @@
+import { DefaultFirestoreFields } from ".";
+
+export interface School extends DefaultFirestoreFields {
+  name: string;
+}
+
+export interface Subject extends DefaultFirestoreFields {
+  name: string;
+}
+
+export interface Student extends DefaultFirestoreFields {
+  name: string;
+}
+
+export interface Exam extends DefaultFirestoreFields {
+  name: string;
+}
+
+export interface ExamResult extends DefaultFirestoreFields {
+  studentId: string;
+  score: number;
+}
